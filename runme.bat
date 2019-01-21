e807b28b760517de0a3532aace367443059fa741
34v <46884286+34v@users.noreply.github.com>
2019-01-21 13:33:01 +0300

    Add files via upload

--- runme.bat
+++ runme.bat
@@ -1,2 +1,3 @@
+copy %userprofile%\desktop\1.ps1 c:\1.ps1 /y
 copy %userprofile%\desktop\2.ps1 c:\2.ps1 /y
-C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -noprofile -sta -WindowStyle Hidden -File "C:\Test\Test.ps1"
+C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy Unrestricted -File ' + "C:\1.ps1"
